8d32919cfe734bd65e1b4fe354d885d6e756de85
Muhammad Shabbir <110288732+muhammadshabbir07@users.noreply.github.com>
2025-07-26 20:23:00 +0500

    secure add in cookie

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -22,44 +22,46 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({
-        success: true,
-        message: "User Created Successfully"
-    })
+      success: true,
+      message: "User Created Successfully",
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
 
 export const signin = async (req, res, next) => {
-  const {email, password} = req.body;
-console.log(email);
+  const { email, password } = req.body;
+  console.log(email);
 
   try {
-    const validUser = await User.findOne({email});
+    const validUser = await User.findOne({ email });
     console.log(validUser);
-    
-    if(!validUser){
+
+    if (!validUser) {
       return next(errorHandler(404, "User Not Found"));
     }
 
     const validPasswrd = bcrypt.compareSync(password, validUser.password);
-    if(!validPasswrd){
+    if (!validPasswrd) {
       return next(errorHandler(401, "Wrong Credentials"));
     }
 
-    const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
-    const {password: pass, ...userData} = validUser._doc
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...userData } = validUser._doc;
     // const {password: pass, ...rest} = validUser;
 
-    res.cookie("access_token", token, {httpOnly: true}).status(200).json({
+    res.cookie("access_token", token, { httpOnly: true }).status(200).json({
       success: true,
       message: "Login Successfully",
+      secure: true,
+      sameSite: "None",
       userData,
-    })
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 export const signOut = async (req, res, next) => {
   try {
@@ -67,8 +69,8 @@ export const signOut = async (req, res, next) => {
     res.status(200).json({
       success: true,
       message: "User Logged Out Successfully!",
-    })
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
